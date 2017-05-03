43dd08a73b712fd3b6a7bcb3cff8839f9e243714
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-05-03 17:09:37 -0400

    docker compose

---
+++ Cluster.WebCrawler/src/build-docker-images.ps1
@@ -0,0 +1,3 @@
+﻿docker build -f .\WebCrawler.Service\Dockerfile .\WebCrawler.Service -t nickc/trackingservice:latest
+docker build -f .\WebCrawler.CrawlService\Dockerfile .\WebCrawler.CrawlService -t nickc/crawlservice:latest
+docker build -f .\WebCrawler.Web\Dockerfile .\WebCrawler.Web -t nickc/webcrawlerweb:latest
